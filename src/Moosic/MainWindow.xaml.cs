6f3fb4089968da1b1396b61113a5c652269ecb62
Luis Antunes <lr.antunes.dev@gmail.com>
2011-04-03 12:19:56 +0100

    Initial Commit

---
+++ src/Moosic/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Rovidi.Moosic.ViewModel;
+
+namespace Rovidi.Moosic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
